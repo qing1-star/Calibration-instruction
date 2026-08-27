38a4c826fbed4e58de4da11e9a1653732e2c34ff
13619258079@163.com <13619258079@163.com>
2026-08-27 10:15:45 +0800

    Add calibration and ABB instruction translation algorithm

---
+++ TargetConfigSetting.cmake
@@ -0,0 +1,10 @@
+##########################################################################
+# CMake requirement
+##########################################################################
+cmake_minimum_required(VERSION 3.20)
+
+set(${TARGET_NAME}_RequiredLibsPublic
+    SMRobotSpray::RotationBodyTrajectoryPlanning
+)
+
+set(${TARGET_NAME}_RequiredLibsPrivate)
